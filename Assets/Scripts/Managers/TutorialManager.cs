5d17114edc7461dd3cd47643a9abf6f486a7cf17
Mateo Conde <mateoconde30@gmail.com>
2024-02-26 12:09:06 +0100

    Tutoriales bonicos metidos

--- Assets/Scripts/Managers/TutorialManager.cs
+++ Assets/Scripts/Managers/TutorialManager.cs
@@ -12,8 +12,9 @@ public class TutorialManager : MonoBehaviour
     private GameManager manager;
     [SerializeField] private MouseManager mouseManager;
     private SceneManagement pause;
+    private CardEffectManager effectManager;
 
-    private bool gamepaused, condition;
+    private bool gamepaused, condition, wasActive;
     private bool[] tutorialTriggered;
 
     public TextAndImage[] tutorialPackages;
@@ -25,8 +26,9 @@ public class TutorialManager : MonoBehaviour
     {
         manager = GameManager.Instance;
         pause = SceneManagement.Instance;
+        effectManager = CardEffectManager.Instance;
         startTimer = 2;
-        tutorialTriggered = new bool[6];
+        tutorialTriggered = new bool[7];
         mouseManager.canClick = false;
         pause.canPause = false;
     }
@@ -52,13 +54,15 @@ public class TutorialManager : MonoBehaviour
         }
 
         #region Tutorial Triggers
-        if (manager.currentState == GameManager.turnState.Movecard && !tutorialTriggered[1])
+        if(!tutorialTriggered[1])
+        if (manager.currentState == GameManager.turnState.Movecard)
         {
             StopGame(1);
             StopGame(2);
         }
 
-        if(manager.trapTriggered && !tutorialTriggered[3])
+        if(!tutorialTriggered[3])
+        if(manager.trapTriggered)
         {
             StopGame(3);
         }
@@ -68,14 +72,27 @@ public class TutorialManager : MonoBehaviour
             condition = true;
         }
 
-        if(condition && !manager.moveCardToHand && !tutorialTriggered[4])
+        if(!tutorialTriggered[4])
+        if(condition && !manager.moveCardToHand)
         {
             StopGame(4);
         }
 
-        if(manager.turnCount == 4 && !tutorialTriggered[5])
+        if (!tutorialTriggered[5])
         {
-            StopGame(5);
+            if (manager.turnCount == 4)
+            {
+                StopGame(5);
+            }
+        }
+
+        if (!tutorialTriggered[6])
+        {
+            if (effectManager.paymentMenu.activeInHierarchy)
+            {
+          
+                StopGame(6);
+            }
         }
         #endregion
 
@@ -103,7 +120,16 @@ public class TutorialManager : MonoBehaviour
             Time.timeScale = 0;
             gamepaused = true;
             blackBox.SetActive(true);
-            blackScreen.SetActive(true);
+            if(blackScreen.activeInHierarchy)
+            {
+                wasActive = true;
+            }
+            else
+            {
+                blackScreen.SetActive(true);
+                wasActive = false;
+            }
+            
             activeMenus++;
             pause.canPause = false;
 
@@ -141,7 +167,10 @@ public class TutorialManager : MonoBehaviour
         {
             if(manager.newCardSlot != null) manager.newCardSlot.transform.GetChild(0).GetComponent<SpriteRenderer>().sortingOrder = 20;
             blackBox.SetActive(false);
-            blackScreen.SetActive(false);
+            if(!wasActive)
+            {
+                blackScreen.SetActive(false);
+            }
             displayImage.enabled = false;
             textBox.gameObject.SetActive(false);
             pause.canPause = true;
