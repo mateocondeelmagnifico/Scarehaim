97516886395fddf17808835966f28eb954096795
Mateo <mateoconde30@gmail.com>
2024-10-05 13:10:00 +0200

    Bug turncounter arregglao

--- Assets/Scripts/Other Behaviors/TurnCheck.cs
+++ Assets/Scripts/Other Behaviors/TurnCheck.cs
@@ -12,8 +12,8 @@ public class TurnCheck : MonoBehaviour
     public int turnsUntilEnemy;
     private int costumeTurns;
 
-    private bool move;
-    private bool[] hasArrived = new bool[5];
+    private bool move, storeMove;
+    private bool[] hasArrived = new bool[6];
 
 
     private void Update()
@@ -21,12 +21,29 @@ public class TurnCheck : MonoBehaviour
         //Move when turn progresses
         if (move)
         {
-            for (int i = 4; i >= 0; i--)
+            for (int i = 5; i >= 0; i--)
             {
                //Move towards objective and decelerate when near
                Vector3 currentDestination = positions[i].GetComponent<DestinationInfo>().destination;
-                if (positions[i].position != currentDestination) positions[i].position = Vector3.MoveTowards(positions[i].position, currentDestination, 1 * Time.deltaTime);
-                else hasArrived[i] = true;             
+                if (positions[i].position != currentDestination)
+                {
+                    positions[i].position = Vector3.MoveTowards(positions[i].position, currentDestination, 1 * Time.deltaTime);
+
+                    if(i == 0)
+                    {
+                        if (Vector3.Distance(positions[i].position, currentDestination) < 0.32f) positions[i].GetComponent<SpriteRenderer>().enabled = false;
+                    }
+
+                    if (i == 5)
+                    {
+                        if (Vector3.Distance(positions[i].position, currentDestination) < 0.32f) positions[i].GetComponent<SpriteRenderer>().enabled = true;
+                    }
+                }
+                else 
+                {
+                    hasArrived[i] = true;
+                }
+               
             }
 
             #region Check if all have arrived
@@ -37,21 +54,34 @@ public class TurnCheck : MonoBehaviour
                 if (hasArrived[i]) count++;
             }
 
-            if (count == 5) move = false;
+            if (count == 6) move = false;
             #endregion
+        }
+
+        //This is so that moving left doesn't happen 2 times in a row
+        if(storeMove && !move)
+        {
+            MoveLeft();
+            storeMove = false;
         }
     }
 
     public void MoveLeft()
     {
-        for(int i = 4; i >= 0; i--)
+        if (move)
+        {
+            storeMove = true;
+            return;
+        }
+
+        for(int i = 5; i >= 0; i--)
         {
             #region Inform images where to go
             if (i == 0)
             {
                 Transform firstIcon = positions[i];
                 firstIcon.position = startPos.position;
-                firstIcon.GetComponent<DestinationInfo>().destination = startPos.position;
+                firstIcon.GetComponent<DestinationInfo>().destination = startPos.position - new Vector3(0.66f,0,0);
                 positions.RemoveAt(0);
                 positions.Add(firstIcon);     
             }
@@ -59,7 +89,7 @@ public class TurnCheck : MonoBehaviour
             #endregion
 
             #region Decide if it's either player or enemy sprite
-            if (i == 4)
+            if (i == 5)
             {
                 if (turnsUntilEnemy > 0)
                 {
