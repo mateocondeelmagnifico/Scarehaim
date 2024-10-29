7c9e494fce7cfbae29e8715be148b405cd596180
Mateo <mateoconde30@gmail.com>
2024-10-29 20:52:24 +0100

    Mini mejoras

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -487,7 +487,6 @@ public class Movement : MonoBehaviour
         mouseManager.hasTreat = true;
         mouseManager.DeactivateRadar();
     }
-
     private bool CanGoThere(Vector2 pos)
     {
         bool istrue = false;
