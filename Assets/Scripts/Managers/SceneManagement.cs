b3b7b2f0dbf9262c232f6283d5c76566563ec26d
Mateo <mateoconde30@gmail.com>
2024-11-17 13:52:29 +0100

    arreglado bug menú pausa

--- Assets/Scripts/Managers/SceneManagement.cs
+++ Assets/Scripts/Managers/SceneManagement.cs
@@ -84,6 +84,25 @@ public class SceneManagement : MonoBehaviour
     }
     public void ExitMenu(GameObject menu)
     {
+        #region Delete arrows from exit menu
+        //If you hover a button and press esc in this menu the arrows are not deactivated normally
+        //so this finds the arrows code deactivates them
+        if (currentMenu.name == "Pause Menu")
+        {
+            for (int i = 0; i < currentMenu.transform.childCount; i++)
+            {
+                if(currentMenu.transform.GetChild(i).childCount > 0)
+                {
+                    Transform myButton = currentMenu.transform.GetChild(i);
+                    for (int e = 0; e < myButton.childCount; e++)
+                    {
+                        if(myButton.GetChild(e).GetComponent<Image>()) myButton.GetChild(e).gameObject.SetActive(false);
+                    }
+                }
+            }
+        }
+        #endregion
+
         currentMenu = null;
         menu.SetActive(false);
 
